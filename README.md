c502a228476021d937ee72c4046a081046fdbc56
lantern77 <peter_hoang2@hotmail.com>
2018-12-22 13:33:25 -0500

    Setup paperjs testrun

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # 3d-origami-diagram-tool
 Tool for drawing 3d origami diagrams
+
+###To quickly run dev server
+install http-server
+`npm install http-server -g`
+Then cd into the folder of interest and run http-server
+
+### Dependencies
+* paperjs
+* jquery
+
+**Small note I'm using paperjs directly with javascript so there are a few caveats**
+* Register a handler for when the DOM is ready, since we cannot work with the canvas before that.
+* Tell the paper object to setup a Project and a View for our canvas. Instead of passing the canvas object, we can also pass the canvas element's ID as a String. In PaperScript, this happens automatically through the canvas="ID" attribute.
+* Access all Paper.js classes and objects through the paper object, since they are no longer global.
+* Use Math functions instead of operators on Point and Size.
+* Draw the view at the end, since that is now only automatically handled when a view.onFrame handler is installed.
+
+### Note the tool has only been tested on Google Chrome and Firefox
---
+++ editor.js
@@ -0,0 +1,47 @@
+import test from './components/test.js'
+
+// Note that the paper object is a global object
+$(document).ready(() =>{
+
+    // Get a reference to the canvas object
+    var canvas = $('#origami-editor')[0];
+    if (canvas === undefined) {
+			alert('Could not retrieve canvas, origami editor')	
+			return 
+    }
+
+    // Create an empty project and a view for the canvas:
+    paper.setup(canvas);
+
+    //dimensions of grid (e.g. a 5x10 grid)
+    var rows = 5;
+    var cols = 10;
+
+    //center coordinates of any circle being drawn
+    var x;
+    var y;
+
+    //center coordinates of first circle being drawn
+    var xstart = 50;
+    var ystart = 50;
+
+    // distance between center coordinates
+    var xspacing = 100;
+    var yspacing = 100;
+
+    var radius = 10;
+    var fillColor = 'orange';
+
+    // create grid and draw circles
+    for (var row = 1; row <= rows; row++) {
+        y = ystart + (row - 1) * yspacing;
+
+        for(var col = 1; col <= cols; col++) {
+            x = xstart + (col - 1) * xspacing;
+            new paper.Path.Circle(new paper.Point(x, y), radius).fillColor = fillColor;
+        }
+    }
+
+    // Draw the view now:
+    paper.view.draw();
+}) 
