b130ab98543b7af8fc512de8f4cacf0f59075055
lantern77 <peter_hoang2@hotmail.com>
2018-12-26 12:34:33 -0500

    Added zoom feature onto button within toolbox

--- components/tools/magnify.js
+++ components/tools/magnify.js
@@ -8,6 +8,12 @@ export default {
   // tool object that is stored here, we will activate or turn this
   // on appropriately
   tool: null,
+
+  /**
+   * Initialize the tool on start of the program
+   * Install event-listener for tool-box
+   * and create tool
+   */
   init () {
     // Add event-listener for tools
     this.toolListeners()
@@ -20,6 +26,11 @@ export default {
 
         // bind box to cursor
         $(document).on('mousemove', this.popover)
+
+        // shift key update icon
+        $(document).on('keydown', this.holdShift)
+        $(document).on('keyup', this.releaseShift)
+
         $('#popover').show()
       } else {
         this.deActivateTool()
@@ -32,8 +43,10 @@ export default {
     $('#magnify-plus').removeClass('pure-button-active')
     $('#origami-editor').css('cursor', 'pointer')
 
-    // remove box from cursor
+    // remove box from cursor and additional event-listeners
     $(document).off('mousemove', this.popover)
+    $(document).off('keydown', this.holdshift)
+    $(document).off('keyup', this.releaseShift)
     $('#popover').hide()
     paper.tool = null
   },
@@ -46,26 +59,59 @@ export default {
     this.tool = new paper.Tool()
     this.tool.name = 'toolPath'
 
-    let path
-
     this.tool.onMouseDown = (event) => {
       // if left click
       if (event.event.which === 1) {
-        path = new paper.Path()
-        path.strokeColor = '#424242'
-        path.strokeWidth = 4
-        path.add(event.point)
+        if (event.modifiers.shift === true) {
+          paper.view.zoom = this.changeZoom(-1)
+        } else {
+          paper.view.zoom = this.changeZoom(1)
+        }
       } else if (event.event.which === 3) {
         this.deActivateTool()
       }
     }
 
-    this.tool.onMouseDrag = function (event) {
-      path.add(event.point)
-    }
     // setting this to null makes it inactive on start
     // this is global active tool scope
     paper.tool = null
+  },
+
+  /* TODO add pan and zoom
+  https://stackoverflow.com/questions/32540165/how-to-pan-using-paperjs
+  https://matthiasberth.com/tech/stable-zoom-and-pan-in-paperjs */
+  /**
+   * Changes the zoom level of the current view
+   * @param {Number} delta contains either a negative or positive number indicates which direction to zoom
+   * negative is closer, positive is outward
+   */
+  changeZoom (delta) {
+    let factor = 1.05
+    if (delta < 0) {
+      return paper.view.zoom / factor
+    } else {
+      return paper.view.zoom * factor
+    }
+  },
+
+  /**
+   * Function for when we hold shift with this tool
+   * @param {event} event
+   */
+  holdShift (event) {
+    if (event.shiftKey === true) {
+      $('#origami-editor').css('cursor', 'zoom-out')
+    }
+  },
+
+  /**
+   * Function for when we hold shift with this tool
+   * Note can't determine the event key type
+   * It also only updates after mouse event or moves
+   * @param {event} event
+   */
+  releaseShift (event) {
+    $('#origami-editor').css('cursor', 'zoom-in')
   },
 
   /**
