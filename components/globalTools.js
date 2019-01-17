5db429d1570d7f09d04888301d609e673ada1ea8
lantern77 <peter_hoang2@hotmail.com>
2019-01-16 21:42:24 -0500

    Added ctrl+z shortcut for undo

--- components/globalTools.js
+++ components/globalTools.js
@@ -13,8 +13,16 @@ export default {
   eventListeners () {
     $('#undo-button').on('click', function () {
       let lastAction = actionStack.undoStack.pop()
-      lastAction.undo()
-      console.log(grid)
+      if (lastAction !== undefined) {
+        lastAction.undo()
+      }
+    })
+
+    // add keyboard shortcut for ctrl+z/undo
+    $(document).on('keydown', function (e) {
+      if (e.ctrlKey && (e.which === 90)) {
+        $('#undo-button').trigger('click')
+      }
     })
   }
 }
