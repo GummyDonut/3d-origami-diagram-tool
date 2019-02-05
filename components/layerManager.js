875ba229570e290a9c917e886fc6c964fa2543aa
lantern77 <peter_hoang2@hotmail.com>
2019-02-04 20:24:23 -0500

    Start the progress of drawing layers

--- components/layerManager.js
+++ components/layerManager.js
@@ -11,17 +11,45 @@ export default {
         'of': $('#tool-options')
       },
       'width': 200,
-      'height': 400
+      'height': 400,
+      'minHeight': 260
     })
 
     $('#layer-manager').on('dialogclose', function (event) {
       $('#tool-bar-showlayers > i').hide()
     })
 
-    // access layers through here
-    // http://paperjs.org/reference/project/#layers
-    // http://paperjs.org/reference/layer/
-    // update name
+    this.draw()
+  },
+
+  /**
+   * Update the layer manager
+   */
+  draw () {
+    // Get the layers
+    let layers = paper.project.layers
+
+    if (layers === undefined) {
+      console.log('There is an error, we lost reference to layers')
+    }
+    // If there is only one layer and has no name set a default name
+    if (layers.length === 1 && layers[0].name === null) {
+      layers[0].name = 'background'
+    }
+
+    // clear out content
+    // attempt to possibly keep all event-listeners
+    let container = $('#layer-manager div.layer-container')
+    container.empty()
+
+    // loop through layers and add
+    layers.forEach((layer) => {
+      // TODO CREATE COMPONENT HERE TO INSERT
+      container.append(layer.name)
+    })
+
+    // weird quirk updating the
+    // http://paperjs.org/tutorials/project-items/project-hierarchy/#accessing-children-by-name
     console.log(paper.project)
   }
 }
